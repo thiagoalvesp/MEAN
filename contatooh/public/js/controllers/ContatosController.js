b70b8f9544f97f39b5bb9bbdc951f72939d5f06b
thiagoalvesp <thiago-alvesp@live.com>
2016-10-26 23:02:10 -0200

    Pagina 97

--- contatooh/public/js/controllers/ContatosController.js
+++ contatooh/public/js/controllers/ContatosController.js
@@ -1,28 +1,12 @@
-<<<<<<< HEAD
 angular.module('contatooh').controller('ContatosController',function($scope, $http) {
    
    $scope.contatos = [];
-=======
-angular.module('contatooh').controller('ContatosController',function($scope) {
-   
-   $scope.contatos = [
-    {_id:1,nome:'Contato Exemplo 1',email:'cont1@empresa.com.br'},
-    {_id:2,nome:'Contato Exemplo 2',email:'cont2@empresa.com.br'},
-    {_id:3,nome:'Contato Exemplo 3',email:'cont2@empresa.com.br'}
-   ];
->>>>>>> 4aed1c6aeb51175eed9a48aad02c74f113b1c349
 
-   $scope.total = 0; 
 
    $scope.filtro = '';
 
-   $scope.incrementa = function () {
-     $scope.total++;  
-   };
-
-<<<<<<< HEAD
    $http.get('/contatos')
-   .sucess(function(data){
+   .success(function(data){
      $scope.contatos = data;
    })
    .error(function(statusText){
@@ -30,6 +14,5 @@ angular.module('contatooh').controller('ContatosController',function($scope) {
      console.log(statusText);
    });
 
-=======
->>>>>>> 4aed1c6aeb51175eed9a48aad02c74f113b1c349
+
 });
