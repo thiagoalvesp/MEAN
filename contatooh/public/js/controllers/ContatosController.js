39b9a695289af571f962b8ffc5bb8ecc8323c861
thiagoalvesp <thiago-alvesp@live.com>
2016-10-26 22:50:09 -0200

    Algumas Alterações - pag 96

--- contatooh/public/js/controllers/ContatosController.js
+++ contatooh/public/js/controllers/ContatosController.js
@@ -1,10 +1,6 @@
-angular.module('contatooh').controller('ContatosController',function($scope) {
+angular.module('contatooh').controller('ContatosController',function($scope, $http) {
    
-   $scope.contatos = [
-    {_id:1,nome:'Contato Exemplo 1',email:'cont1@empresa.com.br'},
-    {_id:2,nome:'Contato Exemplo 2',email:'cont2@empresa.com.br'},
-    {_id:3,nome:'Contato Exemplo 3',email:'cont2@empresa.com.br'}
-   ];
+   $scope.contatos = [];
 
    $scope.total = 0; 
 
@@ -13,5 +9,14 @@ angular.module('contatooh').controller('ContatosController',function($scope) {
    $scope.incrementa = function () {
      $scope.total++;  
    };
+
+   $http.get('/contatos')
+   .sucess(function(data){
+     $scope.contatos = data;
+   })
+   .error(function(statusText){
+     console.log("Não foi possível obter a lista de contatos");
+     console.log(statusText);
+   });
 
 });
